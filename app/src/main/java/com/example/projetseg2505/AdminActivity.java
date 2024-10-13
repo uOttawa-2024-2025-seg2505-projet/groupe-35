91d7c236241d94af74e633db8a1f7e2a4d9cea88
Othmane Ouajjou <ouajjouothmane@gmail.com>
2024-10-13 12:14:19 -0400

    test pour othnag

--- app/src/main/java/com/example/projetseg2505/AdminActivity.java
+++ app/src/main/java/com/example/projetseg2505/AdminActivity.java
@@ -1,12 +1,20 @@
 package com.example.projetseg2505;
 
 import android.os.Bundle;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.badge.BadgeUtils;
+
 public class AdminActivity extends AppCompatActivity {
+
+    private EditText input;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin); // Ensure you have a layout file named activity_admin.xml
+
     }
 }
+
